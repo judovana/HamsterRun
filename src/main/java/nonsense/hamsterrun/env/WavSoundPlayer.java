74782d82719efe87e86bf32a6201085b18e5db84
Jiri <jvanek@redhat.com>
2025-04-19 21:07:50 +0200

    Attempt to control the volume, not working...

--- src/main/java/nonsense/hamsterrun/env/WavSoundPlayer.java
+++ src/main/java/nonsense/hamsterrun/env/WavSoundPlayer.java
@@ -5,7 +5,10 @@ import nonsense.hamsterrun.sprites.SpritesProvider;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.sound.sampled.FloatControl;
+import javax.sound.sampled.Line;
 import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.Mixer;
 import javax.sound.sampled.UnsupportedAudioFileException;
 import java.io.IOException;
 import java.net.URL;
@@ -33,10 +36,34 @@ public class WavSoundPlayer {
     }
 
     public static void rawPlay(String what) throws IOException, LineUnavailableException, UnsupportedAudioFileException {
+        WavSoundPlayer.rawPlayAsync("turbo");
+    }
+    public static Clip rawPlay(String what, float volume) throws IOException, LineUnavailableException, UnsupportedAudioFileException {
         URL runUrl = SpritesProvider.class.getClassLoader().getResource("nonsense/hamsterrun/sounds/effects/" + what + ".wav");
         AudioInputStream audioStream = AudioSystem.getAudioInputStream(runUrl.openStream());
         Clip clip = AudioSystem.getClip();
+//        FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+//        FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.VOLUME);
+//        float range = gainControl.getMaximum() - gainControl.getMinimum();
+//        float gain = (range * volume) + gainControl.getMinimum();
+//        gainControl.setValue(gain);
         clip.open(audioStream);
         clip.start();
+        return clip;
+    }
+
+    public static void main(String[] args) throws Exception {
+        Mixer.Info[] mixers = AudioSystem.getMixerInfo();
+        System.out.println("There are " + mixers.length + " mixer info objects");
+        for (Mixer.Info mixerInfo : mixers) {
+            System.out.println("mixer name: " + mixerInfo.getName());
+            Mixer mixer = AudioSystem.getMixer(mixerInfo);
+            Line.Info[] lineInfos = mixer.getSourceLineInfo();
+            for (Line.Info lineInfo : lineInfos) {
+                System.out.println("  Line.Info: " + lineInfo);
+            }
+            }
+        Clip clip = rawPlay("turbo", 1.0f);
+        Thread.sleep(clip.getMicrosecondLength() / 1000); // Convert
     }
 }
