6d6ec96583a230586e51e89d1388bd46fb2e4cd8
Jiri Vanek <jvanek@redhat.com>
2025-04-22 18:36:41 +0200

    Now caching the sounds

--- src/main/java/nonsense/hamsterrun/env/WavSoundPlayer.java
+++ src/main/java/nonsense/hamsterrun/env/WavSoundPlayer.java
@@ -9,13 +9,20 @@ import javax.sound.sampled.Line;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.Mixer;
 import javax.sound.sampled.UnsupportedAudioFileException;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 public class WavSoundPlayer {
 
     private final String what;
     private boolean finished = false;
+    private static final Map<String, byte[]> cache = new HashMap<>();
 
     public WavSoundPlayer(String what) {
         this.what = what;
@@ -38,8 +45,13 @@ public class WavSoundPlayer {
     }
 
     public static Clip rawPlay(String what, float volume) throws IOException, LineUnavailableException, UnsupportedAudioFileException {
-        URL runUrl = SpritesProvider.class.getClassLoader().getResource("nonsense/hamsterrun/sounds/effects/" + what + ".wav");
-        AudioInputStream audioStream = AudioSystem.getAudioInputStream(runUrl.openStream());
+        byte[] cached = cache.get(what);
+        if (cached == null) {
+            URL runUrl = SpritesProvider.class.getClassLoader().getResource("nonsense/hamsterrun/sounds/effects/" + what + ".wav");
+            cached = streamToBytes(runUrl.openStream());
+            cache.put(what, cached);
+        }
+        AudioInputStream audioStream = AudioSystem.getAudioInputStream(new ByteArrayInputStream(cached));
         Clip clip = AudioSystem.getClip();
 //        FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
 //        FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.VOLUME);
@@ -49,6 +61,20 @@ public class WavSoundPlayer {
         clip.open(audioStream);
         clip.start();//oh, this is actually unblocking....
         return clip;
+    }
+
+    private static byte[] streamToBytes(InputStream is) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        try (is) {
+            byte[] byteChunk = new byte[4096]; // Or whatever size you want to read in at a time.
+            int n;
+            while ((n = is.read(byteChunk)) > 0) {
+                baos.write(byteChunk, 0, n);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return baos.toByteArray();
     }
 
     public static void main(String[] args) throws Exception {
