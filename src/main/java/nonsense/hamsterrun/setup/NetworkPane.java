ed420c88f1e608aa32c1a303b4f28f18e5a24796
Jiri Vanek <jvanek@redhat.com>
2025-06-24 16:13:44 +0200

    Added handshake skeleton

--- src/main/java/nonsense/hamsterrun/setup/NetworkPane.java
+++ src/main/java/nonsense/hamsterrun/setup/NetworkPane.java
@@ -2,9 +2,12 @@ package nonsense.hamsterrun.setup;
 
 
 import java.awt.GridLayout;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.InetAddress;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.Base64;
 import java.util.Random;
 
@@ -25,6 +28,87 @@ public class NetworkPane extends JPanel implements Localized {
 
     private static String hostname = getHostName();
     private static String id = randomStringSimple(10, +1);
+    private int serverPort = findRandomPort();
+
+    private static class ObserverThread extends Thread {
+        private boolean running = true;
+
+        @Override
+        public void run() {
+            while (running) {
+                try {
+
+                } catch (Exception ex) {
+
+                }
+            }
+
+        }
+    }
+
+    ;
+
+    private static class TmpServerThread extends Thread {
+        private boolean running = true;
+        private final int port;
+        private ServerSocket server;
+
+        public TmpServerThread(int port) {
+            this.port = port;
+        }
+
+        @Override
+        public void run() {
+            try (ServerSocket serverSocket = new ServerSocket(port)) {
+                server = serverSocket;
+                while (running) {
+                    try (Socket client = serverSocket.accept(); BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()))) {
+                        String msg = in.readLine();
+                        System.out.println("server got:" + msg);
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                    }
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+
+    private static class SayHelloThread extends Thread {
+        private boolean running = true;
+        private final int port;
+        private ServerSocket server;
+
+        public SayHelloThread(int port) {
+            this.port = port;
+        }
+
+        @Override
+        public void run() {
+            while (running) {
+                try (Socket clientSocket = new Socket("localhost", port); PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);) {
+                    out.println("hello " + id);
+                    Thread.sleep(500);
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+
+            }
+        }
+
+    }
+
+
+    private int findRandomPort() {
+        try (ServerSocket serverSocket = new ServerSocket(0)) {
+            int freePort = serverSocket.getLocalPort();
+            return freePort;
+        } catch (Exception e) {
+            return 8456;
+        }
+    }
 
     private static String getHostName() {
         try {
@@ -58,7 +142,7 @@ public class NetworkPane extends JPanel implements Localized {
         //fixme, move to two on one line
         this.add(new JLabel("this port: "));
         ;
-        this.add(new JSpinner(new SpinnerNumberModel(8456, 1111, 60000, 1)));
+        this.add(new JSpinner(new SpinnerNumberModel(serverPort, 1001, 65000, 1)));
         this.add(new JButton("Create game"));
         this.add(new JLabel("Connected players:"));
         JPanel connected = new JPanel(new GridLayout(0, 1));
@@ -85,9 +169,10 @@ public class NetworkPane extends JPanel implements Localized {
         //fixme, moe to two on one line
         this.add(new JLabel("target port: "));
         ;
-        this.add(new JSpinner(new SpinnerNumberModel(8456, 1111, 60000, 1)));
+        this.add(new JSpinner(new SpinnerNumberModel(serverPort, 1001, 65000, 1)));
         this.add(new JButton("Join game"));
-
+        new TmpServerThread(serverPort).start();
+        new SayHelloThread(serverPort).start();
         setTitles();
     }
 
