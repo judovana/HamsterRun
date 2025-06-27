79e8dd65be313267379b744cde708804c4c33834
Jiri <jvanek@redhat.com>
2025-06-27 20:19:10 +0200

    Client/Server are now properly enabled/disbaled per choice

--- src/main/java/nonsense/hamsterrun/setup/NetworkPane.java
+++ src/main/java/nonsense/hamsterrun/setup/NetworkPane.java
@@ -19,7 +19,11 @@ import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
+import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.BufferedReader;
@@ -39,7 +43,7 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 
-public class NetworkPane extends JPanel implements Localized {
+public class NetworkPane extends JPanel implements Localized, ActionListener {
 
     private static String hostname = getHostName();
     private static String id = randomStringSimple(10, +1);
@@ -53,6 +57,9 @@ public class NetworkPane extends JPanel implements Localized {
     private JLabel connected;
     private JRadioButton serverSelected;
     private JRadioButton clientSelected;
+    private JPanel serverPanel;
+    private JPanel clientPanel;
+    private JLabel titleOfTitles;
 
     private Set<HelloCmd> seenPlayers = new HashSet<>();
 
@@ -250,17 +257,20 @@ public class NetworkPane extends JPanel implements Localized {
 
     public NetworkPane(World world, SetupWindow parent, RatsPanel rats) {
         this.ratsPanel = rats;
-        this.setLayout(new GridLayout(0, 1));
+        this.setLayout(new BorderLayout());
+        serverPanel = new JPanel();
+        serverPanel.setLayout(new GridLayout(0, 1));
         serverSelected = new JRadioButton("<html><div style='text-align: center; font-weight: bold; text-decoration: underline; '>" + "Server" + "</div></html>");
-        this.add(serverSelected);
-        this.add(new JLabel("All guinea pigs created in '" + Localization.get().getRatsTitle() + "' will be used"));
+        serverSelected.addActionListener(this);
+        serverPanel.add(serverSelected);
+        serverPanel.add(new JLabel("All guinea pigs created in '" + Localization.get().getRatsTitle() + "' will be used"));
         //fixme, move to two on one line
-        this.add(new JLabel("this machine: "));
+        serverPanel.add(new JLabel("this machine: "));
         JTextField serverNameFieldToCopy = new JTextField(hostname);
         serverNameFieldToCopy.setEditable(false);
-        this.add(serverNameFieldToCopy);
+        serverPanel.add(serverNameFieldToCopy);
         //fixme, move to two on one line
-        this.add(new JLabel("this port: "));
+        serverPanel.add(new JLabel("this port: "));
         JSpinner serverSpinner = new JSpinner(new SpinnerNumberModel(serverPort, 1001, 65000, 1));
         serverSpinner.addChangeListener(new ChangeListener() {
             @Override
@@ -269,19 +279,27 @@ public class NetworkPane extends JPanel implements Localized {
                 resetPorts();
             }
         });
-        this.add(serverSpinner);
-        this.add(new JLabel("Once all expected guinea pigs are in game, you can press start"));
-        this.add(new JButton("Start"));
-        this.add(new JLabel("Connection status:"));
+        serverPanel.add(serverSpinner);
+        serverPanel.add(new JLabel("Once all expected guinea pigs are in game, you can press start"));
+        serverPanel.add(new JButton("Start"));
+        this.add(serverPanel, BorderLayout.NORTH);
+        titleOfTitles = new JLabel("Connection status:");
+        serverPanel.add(titleOfTitles);
         connected = new JLabel();
         this.add(connected);
+        clientPanel = new JPanel();
+        this.add(clientPanel, BorderLayout.SOUTH);
+        clientPanel.setLayout(new GridLayout(0, 1));
         clientSelected = new JRadioButton("<html><div style='text-align: center; font-weight: bold; text-decoration: underline; '>" + "Client" + "</div></html>");
+        clientSelected.addActionListener(this);
         clientSelected.setSelected(true);
-        this.add(clientSelected);
-        this.add(new JLabel("ALL guinea pigs created in '" + Localization.get().getRatsTitle() + "' will be used"));
-        this.add(new JLabel("id: " + id));
+        kids(clientPanel, true);
+        kids(serverPanel, false);
+        clientPanel.add(clientSelected);
+        clientPanel.add(new JLabel("ALL guinea pigs created in '" + Localization.get().getRatsTitle() + "' will be used"));
+        clientPanel.add(new JLabel("id: " + id));
         //fixme, moe to two on one line
-        this.add(new JLabel("name: "));
+        clientPanel.add(new JLabel("name: "));
         JTextField nameField = new JTextField(randomStringSimple(5, -1));
         nameField.getDocument().addDocumentListener(new DocumentListener() {
             @Override
@@ -307,12 +325,12 @@ public class NetworkPane extends JPanel implements Localized {
             }
         });
         name = nameField.getText();
-        this.add(nameField);
+        clientPanel.add(nameField);
         //fixme, moe to two on one line
-        this.add(new JLabel("target machine: "));
-        this.add(new JTextField("localhost"));
+        clientPanel.add(new JLabel("target machine: "));
+        clientPanel.add(new JTextField("localhost"));
         //fixme, moe to two on one line
-        this.add(new JLabel("target port: "));
+        clientPanel.add(new JLabel("target port: "));
         JSpinner clientSpinner = new JSpinner(new SpinnerNumberModel(clientPort, 1001, 65000, 1));
         clientSpinner.addChangeListener(new ChangeListener() {
             @Override
@@ -321,7 +339,7 @@ public class NetworkPane extends JPanel implements Localized {
                 resetPorts();
             }
         });
-        this.add(clientSpinner);
+        clientPanel.add(clientSpinner);
         ButtonGroup clientServer = new ButtonGroup();
         clientServer.add(serverSelected);
         clientServer.add(clientSelected);
@@ -356,6 +374,28 @@ public class NetworkPane extends JPanel implements Localized {
     @Override
     public void setTitles() {
         setName(Localization.get().getNetTitle());
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (serverSelected.isSelected()) {
+            kids(serverPanel, true);
+            kids(clientPanel, false);
+        }
+        if (clientSelected.isSelected()) {
+            kids(serverPanel, false);
+            kids(clientPanel, true);
+        }
+    }
+
+    private void kids(JPanel panel, boolean b) {
+        Component[] comps = panel.getComponents();
+        for (Component c : comps) {
+            if (!(c instanceof JRadioButton)) {
+                c.setEnabled(b);
+            }
+        }
+        titleOfTitles.setEnabled(true);
     }
 
 }
